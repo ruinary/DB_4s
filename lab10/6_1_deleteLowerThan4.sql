f88dd8a026ca13cfe3711b175f1dd044b0e89b43
Elizabeth <90007395+ruinary@users.noreply.github.com>
2022-09-11 16:30:40 +0300

    Add BD_lab10

---
+++ lab10/6_1_deleteLowerThan4.sql
@@ -0,0 +1,49 @@
+﻿--declare cur cursor local dynamic for 
+--	select p.IDSTUDENT, s.NAME, p.NOTE from PROGRESS p
+--	join STUDENT s on s.IDSTUDENT = p.IDSTUDENT
+--	where p.NOTE < 4
+--		FOR UPDATE
+--declare @id varchar(5), @nm varchar(50), @nt int
+
+
+--open cur
+--fetch cur into @id, @nm, @nt
+--print @id + ': ' + @nm + ' (оценка ' + cast(@nt as varchar) + ')'
+--delete PROGRESS where CURRENT OF cur	-- все удалится
+--delete STUDENT where CURRENT OF cur
+--close cur
+
+insert into PROGRESS (SUBJECT,IDSTUDENT,PDATE, NOTE)
+    values 
+           ('КГ', 16,  '2013-12-04',3),
+           ('КГ', 17,  '2013-12-04',3),
+       ('КГ',   18,  '2013-06-04',3),
+           ('КГ',   19,  '2013-01-04',3),
+           ('КГ',   20,  '2013-01-04',3),
+           ('КГ',   21,  '2013-01-04',3)
+select* from PROGRESS;
+go
+DECLARE @id varchar(10), @name varchar(100), @subj varchar(50), @note varchar(2);
+DECLARE PROGRESS_DELETE_CURSOR CURSOR LOCAL DYNAMIC
+  for SELECT STUDENT.IDSTUDENT, STUDENT.NAME, PROGRESS.SUBJECT, PROGRESS.NOTE from PROGRESS inner join STUDENT on PROGRESS.IDSTUDENT = STUDENT.IDSTUDENT FOR UPDATE;
+OPEN PROGRESS_DELETE_CURSOR
+fetch PROGRESS_DELETE_CURSOR into @id,@name,@subj,@note;
+if(@note < 4)
+      begin
+        DELETE PROGRESS where CURRENT OF PROGRESS_DELETE_CURSOR;
+      end;
+print @id + ' - ' + @name + ' - '+ @subj + ' - ' + @note ;
+While (@@FETCH_STATUS = 0)
+  begin
+    fetch PROGRESS_DELETE_CURSOR into @id,@name,@subj,@note;
+    print @id + ' ▬ ' + @name + ' ▬ '+ @subj + ' ▬ ' + @note ;
+    if(@note < 4)
+      begin
+        DELETE PROGRESS where CURRENT OF PROGRESS_DELETE_CURSOR;
+      end;
+  end;
+close PROGRESS_DELETE_CURSOR;
+
+select* from PRogress;
+--select* from PROGRESS;
+
---
+++ lab10/8_univerTable.sql
@@ -0,0 +1,72 @@
+﻿go
+use UNIVER;
+DECLARE @faculty varchar(150), @pulpit varchar(200), @discipline varchar(10), @discipline_list varchar(200) = '', @qteacher varchar(3), @temp_faculty varchar(50), @temp_pulpit varchar(50);
+SELECT FACULTY.FACULTY_NAME[Факультет],PULPIT.PULPIT_NAME[Кафедра],SUBJECT.SUBJECT[Предмет], count(TEACHER.TEACHER_NAME)[Число преподавателей]
+    from FACULTY inner join PULPIT
+    on PULPIT.FACULTY = FACULTY.FACULTY left outer join SUBJECT
+    on SUBJECT.PULPIT = PULPIT.PULPIT left outer join TEACHER
+    on TEACHER.PULPIT = PULPIT.PULPIT
+    group by FACULTY.FACULTY_NAME,PULPIT.PULPIT_NAME,SUBJECT.SUBJECT order by FACULTY_NAME, PULPIT_NAME asc;
+DECLARE GET_REPORT_CURSOR CURSOR LOCAL STATIC for
+  SELECT FACULTY.FACULTY_NAME,PULPIT.PULPIT_NAME,SUBJECT.SUBJECT, count(TEACHER.TEACHER_NAME)
+    from FACULTY inner join PULPIT
+    on PULPIT.FACULTY = FACULTY.FACULTY left outer join SUBJECT
+    on SUBJECT.PULPIT = PULPIT.PULPIT left outer join TEACHER
+    on TEACHER.PULPIT = PULPIT.PULPIT
+    group by FACULTY.FACULTY_NAME,PULPIT.PULPIT_NAME,SUBJECT.SUBJECT order by FACULTY_NAME, PULPIT_NAME asc;
+
+OPEN GET_REPORT_CURSOR
+FETCH GET_REPORT_CURSOR into @faculty,@pulpit,@discipline,@qteacher;
+while(@@FETCH_STATUS = 0)
+  begin
+    print ' ▬ Факультет: ' + rtrim(@faculty);
+    set @temp_faculty = @faculty;
+    while (@faculty = @temp_faculty)
+      begin
+        print '    ►Кафедра: ' + rtrim(@pulpit);
+        print '      •Количество преподавателей: '+ rtrim(@qteacher) ;
+        set @discipline_list = '      •Дисциплины: ';
+        set @temp_pulpit = @pulpit;
+            if(@discipline != '')
+              begin  
+                if(@discipline_list = '      •Дисциплины: ')
+                  begin
+                    set @discipline_list += @discipline;
+                  end
+                else
+                  begin
+                    set @discipline_list = rtrim(@discipline_list) + ', ' + @discipline;
+                  end;
+              end;
+        FETCH GET_REPORT_CURSOR into @faculty,@pulpit,@discipline,@qteacher;
+        while (@pulpit = @temp_pulpit)
+          begin
+            if(@discipline != '')
+              begin  
+                if(@discipline_list = '      •Дисциплины: ')
+                  begin
+                    set @discipline_list += @discipline;
+                  end
+                else
+                  begin
+                    set @discipline_list = rtrim(@discipline_list) + ', ' + @discipline;
+                  end;
+              end;
+            FETCH GET_REPORT_CURSOR into @faculty,@pulpit,@discipline,@qteacher;
+          end;
+        if(@discipline_list != '      •Дисциплины: ')
+          begin
+            print rtrim(@discipline_list) ;
+            set @discipline_list = '      •Дисциплины: ';
+          end
+        else
+          begin
+            print rtrim(@discipline_list) + ' нет' ;
+          end;
+          if(@@FETCH_STATUS != 0)
+            begin
+              break;
+            end;
+      end;
+  end;
+CLOSE GET_REPORT_CURSOR;
